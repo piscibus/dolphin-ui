30e44b48c5b65f5b070e06ae50fe73ad9943a4d4
abdelrahman-waziry <abdelrahman.waziry@gmail.com>
2021-03-24 20:39:09 +0200

    Implement setLocale functionality

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 import 'antd/dist/antd.css'
 import './css/style.css'
 import Routes from './routes/index'
-import store from './store/store';
+import store from './store/store'
+import { setLocale } from './modules/Authentication/store/actions';
+
+store.dispatch(setLocale('en'))
 
 function App() {
   return (
     <div className="App">
-      <Provider store={store()}>
+      <Provider store={store}>
         <Routes/>
       </Provider>
     </div>
