8af83a9b63faec02421011e726827f95d7bcd212
Amr Ayman <amr67@DESKTOP-9U59ADU>
2021-03-25 20:53:32 +0200

    register ui and switch with login ui

--- src/modules/Authentication/components/Register/Register.js
+++ src/modules/Authentication/components/Register/Register.js
@@ -1,41 +1,45 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { Button, Form, Input, Alert } from 'antd'
 import logo from '../../../../assets/images/logo.png'
 import { LockOutlined, UserOutlined } from '@ant-design/icons'
 const validatePassword = (rule, value, callback) => {
-    console.log(value);
-    if (value && value !== "Secret") {
-      callback("Error!");
+    console.log(value)
+    if (value && value !== 'Secret') {
+        callback('Error!')
     } else {
-      callback();
+        callback()
     }
-  };
+}
 
 const Register = (props) => {
-    const [password, setPassword] = useState("");
-    const [repassword, setRePassword] = useState("");
-    const [isMatched, setPasswordMatch] = useState(true);
-    const repassChange = event => {
-        const target = event.target;
-        const value = target.value;
-        setRePassword(value);
-    }
-    const passChange = event => {
-        const target = event.target;
-        const value = target.value;
-        setPassword(value);
-    }
+    const [formData, setFormData] = useState({
+        password: '',
+        repassword: '',
+        username: '',
+        email: '',
+    })
+    const [isMatched, setPasswordMatch] = useState(true)
+
     const doRegister = () => {
-        if(password !== repassword){
-            setPasswordMatch(false);
+        console.log(formData)
+        if (formData.password !== formData.repassword) {
+            setPasswordMatch(false)
         }
+        //fetch/axios to api
     }
+    const onFormFieldChange = (event) => {
+        const target = event.target
+        const value = target.value
+        const name = target.name
+        formData[name] = value
+        setFormData(formData)
+    }
+    const { switchToLogin } = props
     return (
-        <div className='auth-wrapper'>
         <div className="register-wrapper">
             <Form name="registerForm">
-                <div className='logo'>
-                    <img src={logo} alt="dolphin logo"/>
+                <div className="logo">
+                    <img src={logo} alt="dolphin logo" />
                 </div>
                 <Form.Item
                     rules={[
@@ -48,34 +52,80 @@ const Register = (props) => {
                         size="large"
                         placeholder="Email"
                         className="email-input"
+                        onChange={onFormFieldChange}
+                        name="email"
                     />
                 </Form.Item>
-                <Form.Item rules={[
-                    { required: true, message: 'Please enter your user name' },
-                ]} name="username">
-                    <Input  prefix={<UserOutlined />} placeholder="User Name" size="large" />
+                <Form.Item
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please enter your user name',
+                        },
+                    ]}
+                    name="username"
+                >
+                    <Input
+                        name="username"
+                        prefix={<UserOutlined />}
+                        onChange={onFormFieldChange}
+                        placeholder="User Name"
+                        size="large"
+                    />
                 </Form.Item>
-                <Form.Item rules={[
-                    { required: true, message: 'Please enter your password' },
-                ]} 
-                name="password">
-                    <Input.Password onChange={passChange} prefix={<LockOutlined />} placeholder="Password" size="large" />
+                <Form.Item
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please enter your password',
+                        },
+                    ]}
+                    name="password"
+                >
+                    <Input.Password
+                        name="password"
+                        onChange={onFormFieldChange}
+                        prefix={<LockOutlined />}
+                        placeholder="Password"
+                        size="large"
+                    />
                 </Form.Item>
-                <Form.Item rules={[
-                    { required: true, message: 'Please re-enter your password' },
-                ]} name="repassword">
-                    <Input.Password onChange={repassChange} prefix={<LockOutlined />} placeholder="Re-Enter Password" size="large" />
+                <Form.Item
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please re-enter your password',
+                        },
+                    ]}
+                    name="repassword"
+                >
+                    <Input.Password
+                        name="repassword"
+                        onChange={onFormFieldChange}
+                        prefix={<LockOutlined />}
+                        placeholder="Re-Enter Password"
+                        size="large"
+                    />
                 </Form.Item>
-                {!isMatched && <Form.Item>
-                    <Alert message="Passwords Mismatch" type="error" />
-                </Form.Item>}
+                {!isMatched && (
+                    <Form.Item>
+                        <Alert message="Passwords Mismatch" type="error" />
+                    </Form.Item>
+                )}
                 <Form.Item>
-                    <Button onClick={doRegister} block type="primary" htmlType="submit">
+                    <Button
+                        onClick={doRegister}
+                        block
+                        type="primary"
+                        htmlType="submit"
+                    >
                         Register
+                    </Button>
+                    <Button type="link" onClick={() => switchToLogin()}>
+                        Already have An Account ? Login
                     </Button>
                 </Form.Item>
             </Form>
-        </div>
         </div>
     )
 }
