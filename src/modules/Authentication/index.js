317e4cc6a5e1de82e3c609e9edb6d9e4d65c8d3d
abdelrahman-waziry <abdelrahman.waziry@gmail.com>
2021-03-25 19:52:30 +0200

    Switch authentication index from class to function component

--- src/modules/Authentication/index.js
+++ src/modules/Authentication/index.js
@@ -1,22 +1,12 @@
-
 import React from 'react'
 import Login from './components/Login/Login'
 
-class index extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-
-        }
-    }
-    
-    render(){
-        return(
-            <div className='auth-wrapper'>
-                <Login/>
-            </div>
-        )
-    }
+const index = (props) => {
+    return (
+        <div className="auth-wrapper">
+            <Login />
+        </div>
+    )
 }
 
 export default index
