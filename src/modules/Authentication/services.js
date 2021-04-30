346062796140e0f3168e5f1597ed411a9b791549
Amr Ayman <amr67@DESKTOP-9U59ADU>
2021-04-30 16:50:30 +0200

    Applying DRY to auth

--- src/modules/Authentication/services.js
+++ src/modules/Authentication/services.js
@@ -10,41 +10,19 @@ const RegisterService = async (
     onfetching()
     let result = false
     try {
-        payload = {
-            ...payload,
-            client_id: process.env.REACT_APP_CLIENT_ID,
-            client_secret: process.env.REACT_APP_CLIENT_SECRET,
-        }
         result = await service.post(
             '/registration/email?include=token',
             payload
         )
     } catch (error) {
         let errorsString = ''
-        const { response } = error
-        if (response) {
-            const { data } = response
-            if (data) {
-                const { errors } = data
-                if (errors.email) {
-                    errorsString += errors.email.join('<br/>')
-                }
-                if (errors.password) {
-                    errorsString += errors.password.join('<br/>')
-                }
-            }
-        }
-        if (errorsString == '') {
-            errorsString = trans('Something Went Wrong')
+        const { inferedMsg } = error
+        if (inferedMsg) {
+            errorsString = trans(inferedMsg)
         }
         onfailure(errorsString)
     }
     if (result) {
-        const { data } = result
-        if (data && data.data) {
-            const { token } = data.data
-            localStorage.setItem('token', JSON.stringify(token))
-        }
         onsuccess(result)
     }
 }
@@ -58,38 +36,16 @@ const LoginService = async (
     onfetching()
     let result = false
     try {
-        payload = {
-            ...payload,
-            client_id: process.env.REACT_APP_CLIENT_ID,
-            client_secret: process.env.REACT_APP_CLIENT_SECRET,
-        }
         result = await service.post('/login/email?include=token', payload)
     } catch (error) {
         let errorsString = ''
-        const { response } = error
-        if (response) {
-            const { data } = response
-            if (data) {
-                const { exception } = data
-                if (
-                    exception &&
-                    exception.includes('InvalidLoginCredentials')
-                ) {
-                    errorsString += trans('Invalid User Or Password')
-                }
-            }
-        }
-        if (errorsString == '') {
-            errorsString = trans('Something Went Wrong')
+        const { inferedMsg } = error
+        if (inferedMsg) {
+            errorsString = trans(inferedMsg)
         }
         onfailure(errorsString)
     }
     if (result) {
-        const { data } = result
-        if (data && data.data) {
-            const { token } = data.data
-            localStorage.setItem('token', JSON.stringify(token))
-        }
         onsuccess(result)
     }
 }
--- src/services/index.js
+++ src/services/index.js
@@ -1,12 +1,50 @@
 const axios = require('axios')
-axios.defaults.baseURL = 'http://localhost/api/v1'
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL
+// Add a request interceptor
+axios.interceptors.request.use(
+    function (config) {
+        config.data = {
+            ...config.data,
+            client_id: process.env.REACT_APP_CLIENT_ID,
+            client_secret: process.env.REACT_APP_CLIENT_SECRET,
+        }
+        return config
+    },
+    function (error) {
+        // Do something with request error
+        return Promise.reject(error)
+    }
+)
 
+axios.interceptors.response.use(
+    function (response) {
+        console.log('resp: ')
+        console.log(response)
+        const { data } = response
+        if (data && data.data && data.data.token) {
+            const {
+                access_token,
+                expires_in,
+                refresh_token,
+                token_type,
+            } = data.data.token
+            localStorage.setItem('access_token', access_token)
+            localStorage.setItem('access_token_expires_in', expires_in)
+            localStorage.setItem('refresh_token', refresh_token)
+            localStorage.setItem('token_type', token_type)
+        }
+        return response
+    },
+    function (error) {
+        const { status, data } = error.response
+        let inferedMsg = 'Something Went Wrong'
+        if (data) {
+            const { exception } = data
+            if (exception && exception.includes('InvalidLoginCredentials')) {
+                inferedMsg = 'Invalid User Or Password'
+            }
+        }
+        return Promise.reject({ status, data, inferedMsg })
+    }
+)
 export default axios
-// const post = async (url, payload) => {
-//     return await axios.post(url, payload)
-// }
-// const get = async (url) => {
-//     return await axios.get(url)
-// }
-
-// export { post, get }
